207b7ca66f77f23afc30274858ed7b0ed70e579f
web3dfi zeus <yesbahia@gmail.com>
2025-07-29 11:47:24 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>Império do Axé está no ar 🌟</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
