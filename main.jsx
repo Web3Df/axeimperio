ee70d722248e86c6b7489867188d7084f06235a0
web3dfi zeus <yesbahia@gmail.com>
2025-07-29 10:12:05 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Império do Axé está online 🎉</h1>
+  </React.StrictMode>
+)
